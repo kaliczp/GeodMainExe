6d50f03411515b2ba71b89333ad3118b78f054a1
Péter Kalicz <kaliczp@gmail.com>
2022-02-22 07:16:24 +0100

    Test calculates based on generated data.frame
    Test script now calculates the given line of the generated
    data.frame. It helps control results

--- test.R
+++ test.R
@@ -1,12 +1,13 @@
 ## Test first geod
 
-delta <- c(28,35,22)
+tti <- 1
+delta.simp <- paste(GeodI.df[tti,c("deg","min","sec")], collapse = "-")
 
-delta.fok <- simple.to.deg(delta)
+delta.fok <- simple.to.deg(GeodI.df[tti,c("deg","min","sec")])
 
-ttcoord <- data.frame(x = 268823.12, y = 392123.25)
+ttcoord <- data.frame(x = GeodI.df[tti,"x"], y = GeodI.df[tti,"y"])
 
-(ttircoord <- first.geod(ttcoord, delta.fok, 82.23))
+(ttircoord <- first.geod(ttcoord, delta.fok, GeodI.df[tti,c("dist")]))
 
 ## Test second geod
 (dist.bear <- second.geod(ttcoord, ttircoord))
