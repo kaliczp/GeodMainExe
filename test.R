7227cef890dec8933dee83bda820fc1999d87d3b
Péter Kalicz <kaliczp@gmail.com>
2022-02-22 18:04:28 +0100

    Second excercise test for df
    Now second excercise test is rewritten to data.frame

--- test.R
+++ test.R
@@ -10,7 +10,8 @@ ttcoord <- data.frame(x = GeodI.df[tti,"x"], y = GeodI.df[tti,"y"])
 (ttircoord <- first.geod(ttcoord, delta.fok, GeodI.df[tti,c("dist")]))
 
 ## Test second geod
-(dist.bear <- second.geod(ttcoord, ttircoord))
 
+tti <- 20
+(dist.bear <- second.geod(GeodII.df[tti,c("x","y")], GeodII.df[tti,c("x2","y2")]))
 deg.to.simple(deg.angle = dist.bear$angle)
 
