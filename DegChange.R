cc14906c5b7e6d020647ebd31bbcef4e780ceb3d
Péter Kalicz <kaliczp@gmail.com>
2022-02-22 07:13:23 +0100

    Better handle data.frame source
    If the source is a data.frame the final step remove properties
    and result a simple numeric number instead data.frame.

--- DegChange.R
+++ DegChange.R
@@ -1,5 +1,6 @@
 simple.to.deg <- function(simple.angle) {
-    simple.angle[1] + (simple.angle[2] + simple.angle[3] / 60 ) / 60
+    deg <- simple.angle[1] + (simple.angle[2] + simple.angle[3] / 60 ) / 60
+    as.numeric(deg)
 }
 
 deg.to.simple <- function(deg.angle) {
