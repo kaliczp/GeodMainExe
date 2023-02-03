1ee9ef438689ad5781f899d24ef097daf199831d
Péter Kalicz <kaliczp@gmail.com>
2023-02-03 21:15:35 +0100

    Better similarity to real microscope
    Text is raised above ticks, aspect ratio modified, bottom margin decreased.

--- beoszt.R
+++ beoszt.R
@@ -1,9 +1,11 @@
 beoszt <- function(deg) {
     hatar <- data.frame(fok = c(-10,70), lat = c(0,30))
-    plot(lat ~ fok, hatar, asp = TRUE, axes = FALSE,
+    plot(lat ~ fok, hatar, asp = 0.7, axes = FALSE,
          type = "n", xlab = "", ylab = "")
-    for(akt in 0:6)
-        axis(1, akt * 10, tck=0.5, lab = akt)
+    for(akt in 0:6) {
+        axis(1, akt * 10, tck=0.5, lab = FALSE)
+    }
+    text((0:6)*10, 20, 0:6)
     for(akt in 0:5)
         axis(1, akt * 10 + 5, tck=0.3, lab = FALSE)
     for(akt in -3:63)
@@ -23,7 +25,7 @@ beoszt <- function(deg) {
 akt.szog <- simple.to.deg(data.frame(deg = 33, min = 22, sec =07))
 
 pdf("beoszt.pdf", paper = "a4")
-par(mfrow=c(5,2), mar = c(2,0,4,0), oma = c(0,0,0,0))
+par(mfrow=c(5,2), mar = c(0,0,4,0), oma = c(0,0,0,0))
 for(tti in 1: 10) {
     akt.values <- c(sample(0:359, 1), sample(0:59, size = 2, replace = TRUE))
     akt.szog <- simple.to.deg(data.frame(deg = akt.values[1], min = akt.values[2], sec = akt.values[3]))
